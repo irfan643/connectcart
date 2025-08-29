3d9e505a0d11dffff3eb2f038cef28f6e90d839c
rahil <rahil.ikram67@gmail.com>
2025-08-29 14:53:20 +0400

    tabs removed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,57 +1,33 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/tabs/tab1'
-  },
-  {
-    path: '/tabs/',
-    component: TabsPage, 
-    children: [
-      {
-        path: '', 
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/TabPage.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
-  },
   {
     path: '/login',
     component: () => import('@/features/auth/Login.vue')
   },
-     {
- path : '/Forget',
- component :()=> import('@/features/auth/Forget.vue')
+  {
+    path: '/Forget',
+    component: () => import('@/features/auth/Forget.vue')
 
-     },
-
-     {
+  },
+  {
     path: '/SetPassword',
     component: () => import('@/features/auth/SetPassword.vue')
   },
   {
-   path: '/signup',
+    path: '/signup',
     component: () => import('@/features/auth/SignUp.vue')
   },
-  {path :"/Verification",
-    component :()=> import('@/features/auth/Verification.vue')
+  {
+    path: "/Verification",
+    component: () => import('@/features/auth/Verification.vue')
 
-  }
-
+  },
+  {
+    path: '',
+    redirect:"/login"
+  },
 ]
 
 const router = createRouter({
